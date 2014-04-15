14690e270734cfb8bd440c221a5a22f50a0305bc
kizyahaha <kczaja.ingersoll@gmail.com>
2014-04-15 10:37:59 -0500

    Functioning countdown timers on each granularity

--- Kizy_Temp/General_Practice_4.js
+++ Kizy_Temp/General_Practice_4.js
@@ -1,13 +1,13 @@
+"use strict";
 
 
-var foo = setInterval(function(){get_random_num()},5000)
 function get_random_num(){
 	var n = Math.floor(Math.random()*101);
 	n+=950;
 	document.getElementById("num_audience").innerHTML = n;
+	var foo1 = setInterval(get_random_num,5000);
 }
 
-window.onload=get_current_date;
 function get_current_date(){
 	var d=new Date();
 	var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
@@ -16,25 +16,42 @@ function get_current_date(){
 	document.getElementById("date_holder").innerHTML = date;
 }
 
-/*
-var count=60*60*2+5;
-var counter=setInterval(timer, 1000);
-function timer(){
-  count=count-1;
-  if (count <= 0){
-     clearInterval(counter);
-     return;
-  }
-  var hours = Math.floor(count/3600);
-  var minutes = Math.floor((count-hours*3600)/60);
-  var secs = count - hours*3600-minutes*60;
-  document.getElementById("countdown").innerHTML="Next Winner:\n"+hours+":"+minutes+":"+secs;
+
+
+function countdown(){
+	var d = new Date();
+	var curr_hour = d.getHours();
+	var curr_minute = d.getMinutes();
+	var curr_second = d.getSeconds();
+	var hours = 23 - curr_hour;
+	var minutes = 59 - curr_minute;
+	var seconds = 59 - curr_second;
+	minutes = checkTime(minutes);
+	seconds =checkTime(seconds);
+	var daily = document.getElementById("countdown_timer_daily");
+	if (daily)
+		daily.innerHTML = hours + ":" + minutes + ":" + seconds;
+	var hourly = document.getElementById("countdown_timer_hourly");
+	if (hourly)
+		hourly.innerHTML = minutes + ":" + seconds;
+	var minutely = document.getElementById("countdown_timer_minutely");
+	if (minutely)
+		minutely.innerHTML = "0:" + seconds;
+	var foo2 = setInterval(countdown,500);
 }
-*/
+
+
+function checkTime(t){
+	if (t < 10){
+		t = "0" + t;
+	}
+	return t;
+}
+
 
 window.onload = updateSizes;
 function updateSizes(){
 	$('#content').css('height',$('#rant_bubble').height()+150);
 	$('#section_1').css('height',$('#rant_bubble').height());
-	setInterval(function(){updateSizes()},500);
+	var foo3 = setInterval(updateSizes,500);
 }
