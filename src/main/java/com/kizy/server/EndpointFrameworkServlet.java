6dd7126539ed21b43f5076c7ce0955c0b8a164bb
Bill Bindi <bbindi@palantir.com>
2014-04-26 17:22:10 -0700

    Ugly, but working, even for images.

--- src/main/java/com/kizy/server/EndpointFrameworkServlet.java
+++ src/main/java/com/kizy/server/EndpointFrameworkServlet.java
@@ -1,12 +1,15 @@
 package com.kizy.server;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,7 +18,10 @@ import org.springframework.web.servlet.FrameworkServlet;
 
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Closeables;
 import com.google.common.io.Files;
+import com.kizy.web.WebResources;
 
 public class EndpointFrameworkServlet extends FrameworkServlet {
 
@@ -40,11 +46,15 @@ public class EndpointFrameworkServlet extends FrameworkServlet {
     @Override
     protected void doService(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String path = request.getPathInfo();
-        PrintWriter out = response.getWriter();
-        if (path.indexOf('.') == -1) {
+        String extension = WebResources.getExtension(path);
+        if (extension.isEmpty()) {
+            PrintWriter out = response.getWriter();
             outputShim(out, path);
-        } else {
+        } else if (WebResources.TEST_EXTENSIONS.contains(extension)) {
+            PrintWriter out = response.getWriter();
             outputContents(request, path, out);
+        } else if (WebResources.IMAGE_EXTENSIONS.contains(extension)) {
+            outputImage(request, response, path);
         }
     }
 
@@ -63,17 +73,6 @@ public class EndpointFrameworkServlet extends FrameworkServlet {
         out.println(getBodyShim(parseTabNumber(path)));
         out.println("</body>");
         out.println("</html>");
-    }
-
-    private void outputContents(HttpServletRequest request, String path, PrintWriter out) throws IOException {
-        URL url = request.getServletContext().getResource(path);
-        File file;
-        try {
-            file = new File(url.toURI());
-        } catch (URISyntaxException e) {
-            file = new File(url.getPath());
-        }
-        out.println(Files.toString(file, Charsets.UTF_8));
     }
 
     private int parseTabNumber(String path) {
@@ -96,6 +95,31 @@ public class EndpointFrameworkServlet extends FrameworkServlet {
                  "create_contender_space(tab_num);" +
                  "create_footer();" +
                "</script>";
+    }
+
+    private void outputContents(HttpServletRequest request, String path, PrintWriter out) throws IOException {
+        URL url = request.getServletContext().getResource(path);
+        out.println(Files.toString(getFile(url), Charsets.UTF_8));
+    }
+
+    private void outputImage(HttpServletRequest request, HttpServletResponse response, String path) throws IOException {
+        ServletContext servletContext = request.getServletContext();
+        response.setContentType(servletContext.getMimeType(path));
+        FileInputStream in = new FileInputStream(getFile(servletContext.getResource(path)));
+        ServletOutputStream out = response.getOutputStream();
+        ByteStreams.copy(in, out);
+        Closeables.close(in, true);
+        Closeables.close(out, true);
+    }
+
+    private static File getFile(URL url) {
+        File file;
+        try {
+            file = new File(url.toURI());
+        } catch (URISyntaxException e) {
+            file = new File(url.getPath());
+        }
+        return file;
     }
 
     @Override
---
+++ src/main/java/com/kizy/web/WebResources.java
@@ -0,0 +1,26 @@
+package com.kizy.web;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+public class WebResources {
+
+    public static final Set<String> IMAGE_EXTENSIONS = Sets.newHashSet("png",
+                                                                       "jpg",
+                                                                       "jpeg",
+                                                                       "gif");
+
+    public static final Set<String> TEST_EXTENSIONS = Sets.newHashSet("js",
+                                                                      "css",
+                                                                      "html");
+
+    public static String getExtension(String path) {
+        int lastDotIndex = path.lastIndexOf('.');
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return path.substring(lastDotIndex + 1);
+    }
+
+}
