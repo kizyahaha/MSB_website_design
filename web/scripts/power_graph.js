76ce858d4d0554f0832c99f06a8bbcadbdc96521
kizyahaha <kczaja.ingersoll@gmail.com>
2014-06-04 22:38:51 +0200

    Switched the detailed rant graph over to Google Charts as well.  It's still the shit so far.  Billy, don't you dare tell me we shouldn't use it.

--- web/scripts/power_graph.js
+++ web/scripts/power_graph.js
@@ -38,7 +38,7 @@ function create_power_graph(tab_num){
 					   legend:'none',
 					   colors:['rgb(52,52,52)'],
 					   hAxis:{gridlines:{color:'rgb(12,120,154)' , count:-1} , textStyle:{color: 'white' , fontName:'lao ui'} ,
-								baselineColor:'none'},
+								baselineColor:'none' , format:'0'},
 					   vAxis:{title:'Power' , titleTextStyle: {color: 'rgb(52,52,52)' , fontName:'lao ui' , italic:false , bold:true} ,
 								gridlines:{color:'rgb(52,52,52)' , count:5} , textStyle:{color: 'white' , fontName:'lao ui'} , 
 								baselineColor:'rgb(52,52,52)'},
@@ -146,76 +146,7 @@ function create_y_axis(graph){
 	y_axis.render();
 }*/
 
-
-/* Original
-function create_power_graph(tab_num){
-	var power_graph = document.createElement('div');
-	power_graph.id = 'power_graph';
-	document.getElementById('contender_space').appendChild(power_graph);
-}
-
-function update_power_graph(tab_num){
-	create_axis_labels();
-	get_power_scale(tab_num);
-	get_powers(tab_num);
-}
-
-function create_axis_labels(){
-	var y_label = document.createElement('div');
-	y_label.id = 'y_label';
-	y_label.textContent = 'Power';
-	var x_label = document.createElement('div');
-	x_label.id = 'x_label';
-	x_label.textContent = 'Rank';
-	document.getElementById('power_graph').appendChild(y_label);
-	document.getElementById('power_graph').appendChild(x_label);
-}
-
-function get_power_scale(tab_num){
-	var max_power = document.createElement('div');
-	max_power.id = 'max_power';
-	max_power.textContent = '(max)';
-	var min_power = document.createElement('div');
-	min_power.id = 'min_power';
-	min_power.textContent = '(min)';
-	document.getElementById('power_graph').appendChild(max_power);
-	document.getElementById('power_graph').appendChild(min_power);
-}
-
-function get_powers(tab_num){
-	$('<div/>',{id:'power_bars' , width:'100%' , height:'100%'}).appendTo('#power_graph');
-	var graph_width = Math.floor( $('#power_bars').width() );
-	var graph_height = Math.floor( $('#power_bars').height() );
-	var power_bar_width = Math.floor( graph_width*0.75 / num_contenders );
-	var bar_spacing = Math.floor( graph_width*0.25 / (num_contenders+1) );
-	var power_bar_height = Math.floor( graph_height*0.9 );
-	for (i=0 ; i<num_contenders ; i++){
-		var power_bar = document.createElement('div');
-		power_bar.className = 'power_bar';
-		var rank = i+1;
-		power_bar.title = 'rank:' + rank + ', power:' + power_bar_height;
-		power_bar.style.width = power_bar_width;
-		power_bar.style.height = power_bar_height;
-		power_bar_height = Math.floor( power_bar_height * 0.9 );
-		power_bar.style.left = bar_spacing*(i+1) + power_bar_width*i;
-		document.getElementById('power_bars').appendChild(power_bar);
-	}	
-}
-
-function resize_power_bars(){
-	var graph_width = $('#power_bars').width();
-	var power_bar_width = graph_width*0.75 / num_contenders;
-	var bar_spacing = graph_width*0.25 / (num_contenders+1);
-	var power_bars = new Array();
-	var power_bars = document.getElementById('power_bars').children;
-	for (i=0 ; i<power_bars.length ; i++){
-		power_bars[i].style.width = power_bar_width;
-		power_bars[i].style.left = bar_spacing*(i+1) + power_bar_width*i;
-	}
-}
 window.onresize = resize_stuff;
 function resize_stuff(){
 	update_sizes(); //rant bubble
-	resize_detailed_power_graph();
-	resize_power_bars();
-}*/
+}
--- web/scripts/rant_view.js
+++ web/scripts/rant_view.js
@@ -48,167 +48,79 @@ function create_detailed_rant_level(){
 function create_detailed_rant_content(){
 	$('<div/>',{id:'detailed_rant_content'}).appendTo('#detailed_rant_space');
 	$('#detailed_rant_content').text('Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.  Queen of Equestria?  More like Queer of E-queer-stria.');
-	}
+}
 
 function create_detailed_rant_power_graph(){
 	$('<div/>',{id:'detailed_power_graph'}).appendTo('#detailed_rant_container');
-	create_detailed_rant_power_graph_axis_labels();
-	create_detailed_rant_power_graph_power_limits();
-	get_detailed_rant_power_graph();
+
+    google.load('visualization', '1', {packages: ['corechart']});
+	google.setOnLoadCallback(draw_detailed_power_graph);
+    function draw_detailed_power_graph() {
+        var data = new google.visualization.DataTable();
+		data = get_detailed_rant_data();
+
+        var options = {width:$('#detailed_power_graph').width(),
+                       height:$('#detailed_power_graph').height(),
+					   legend:'none',
+					   colors:['rgb(52,52,52)'],
+					   areaOpacity:0.4,
+					   hAxis:{title:'Time' , titleTextStyle: {color: 'rgb(52,52,52)' , fontName:'lao ui' , italic:false , bold:true} ,
+								gridlines:{color:'rgb(52,160,194)' , count:-1} , textStyle:{color: 'white' , fontName:'lao ui'} ,
+								baselineColor:'none' , format:'0'},
+					   vAxis:{title:'Power' , titleTextStyle: {color: 'rgb(52,52,52)' , fontName:'lao ui' , italic:false , bold:true} ,
+								gridlines:{color:'rgb(52,52,52)' , count:5} , textStyle:{color: 'white' , fontName:'lao ui'} , 
+								baselineColor:'rgb(52,52,52)'}, 
+					   backgroundColor: 'transparent',
+					   fontName: 'lao ui',
+					   tooltip:{textStyle:{color:'rgb(52,52,52)' , fontName:'lao ui'}}
+					   }
+
+        var chart = new google.visualization.AreaChart(document.getElementById('detailed_power_graph'));
+		chart.draw(data, options);
+    }
 }
 
-function create_detailed_rant_power_graph_axis_labels(){
-	$('<div/>',{id:'detailed_y_label'}).appendTo('#detailed_power_graph');
-	$('#detailed_y_label').text('Power');
-	$('<div/>',{id:'detailed_x_label'}).appendTo('#detailed_power_graph');
-	$('#detailed_x_label').text('Time');
-}
+function get_detailed_rant_data(){
+	var data = new google.visualization.DataTable();
+	data.addColumn('number', 'Time');
+    data.addColumn('number', 'Power');
+	data.addColumn({type: 'number', role: 'annotation'});
+	data.addColumn({type: 'string', role: 'annotationText'});
+	data.addRows(num_contenders);
 
-function create_detailed_rant_power_graph_power_limits(){
-	$('<div/>',{id:'detailed_max_power'}).appendTo('#detailed_power_graph');
-	$('#detailed_max_power').text('9,437');
-	$('<div/>',{id:'detailed_min_power'}).appendTo('#detailed_power_graph');
-	$('#detailed_min_power').text('0');
-}
-
-function get_detailed_rant_power_graph(){
-	$('<div/>',{id:'detailed_power_bars'}).appendTo('#detailed_power_graph');
-	var detailed_graph_width = $('#detailed_power_bars').width();
-	var detailed_graph_height = $('#detailed_power_bars').height();
-	var curr_power = 500;
-	var curr_point = 0;
-	var max_power = 9437;
-	var events = new Array();
-	events.push(curr_point);
-	for (i=0 ; i<50 ; i++){ //initial rise until jump to 10 minute
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power + get_detailed_random_num(50);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	curr_power += 1000;
-	for (i=0 ; i<50 ; i++){ //rise until leech
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power + get_detailed_random_num(100);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	for (i=0 ; i<25 ; i++){ //leech until "front of the line"
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power - get_detailed_random_num(100);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	while (curr_power<8000){ //rise due to "front of the line" until jump to hourly
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power + get_detailed_random_num(300);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	curr_power+=500;
-	while (curr_power<9437){ //rise due in hourly until decay
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power + get_detailed_random_num(50);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	while (curr_power>7000){ //decay to current
-		var detailed_power_bar = document.createElement('div');
-		detailed_power_bar.className = 'detailed_power_bar';
-		detailed_power_bar.title = 'time:' + curr_point + ', power:' + curr_power;
-		detailed_power_bar.style.height = (curr_power / max_power) * detailed_graph_height;
-		curr_power = curr_power - get_detailed_random_num(100);
-		curr_point++;
-		document.getElementById('detailed_power_bars').appendChild(detailed_power_bar);
-	}
-	events.push(curr_point);
-	
-	var detailed_power_bar_width = detailed_graph_width / (curr_point+1);
-	var change_width = document.getElementsByClassName('detailed_power_bar');
-	var i = change_width.length;
-	while (i--){
-		change_width[i].style.width = detailed_power_bar_width;
-	}
-	var all_bars = document.getElementById('detailed_power_bars').children;
-	for (i=0 ; i<all_bars.length ; i++){
-		all_bars[i].style.left = i*detailed_power_bar_width;
-	}
-	get_detailed_rant_power_graph_events(events , detailed_power_bar_width);
-}
-
-function get_detailed_random_num(range){
-	var n = Math.floor(Math.random()*range);
-	return n;
-}
-
-function get_detailed_rant_power_graph_events(events , bar_width){
-	$('<div/>',{id:'detailed_events'}).appendTo('#detailed_power_graph');
-	var events_length = events.length;
-	for (i=0 ; i<events_length ; i++){
-		var event_marker = document.createElement('div');
-		event_marker.className = 'event_marker';
-		event_marker.style.left = events[i]*bar_width;
-		switch(i){
-			case 0:
-				event_marker.title = 'Rant initialized with 500 power';
-				break;
-			case 1:
-				event_marker.title = 'You won the minutely title!';
-				break;
-			case 2:
-				event_marker.title = 'Queen_of_Equestria used a leech attack!';
-				break;
-			case 3:
-				event_marker.title = 'You employed a "Front of the Pack" item';
-				break;
-			case 4:
-				event_marker.title = 'You won the 10-minute title!';
-				break;
-			case 5:
-				event_marker.title = 'Max power achieved: 9,437';
-				break;
-			case 6:
-				event_marker.title = 'Current power: 7000';
-				break;
-			default:
-				break;
+	var under_9000 = true;
+	var annotation_num = 1;
+	var height = 200;
+	for (var i=0 ; i<num_contenders/2 ; i++){
+		data.setValue(i,0,i+1);
+		data.setValue(i,1,height);
+		if (height > 9000 && under_9000){
+			data.setValue(i,2,annotation_num);
+			data.setValue(i,3,"It's over 9000!");
+			annotation_num++;
+			under_9000 = false;
 		}
-		document.getElementById('detailed_events').appendChild(event_marker);
+		height = Math.floor(height*1.09);
 	}
-}
-
-function resize_detailed_power_graph(){
-	var detailed_graph_width = $('#detailed_power_bars').width();
-	var change_width = document.getElementById('detailed_power_bars').children;
-	var detailed_power_bar_width = detailed_graph_width / (change_width.length);
-	for (i=0 ; i<change_width.length ; i++){
-		change_width[i].style.width = detailed_power_bar_width;
-		change_width[i].style.left = detailed_power_bar_width*i;
+	for (var i=num_contenders/2 ; i<(3*num_contenders/4) ; i++){
+		data.setValue(i,0,i+1);
+		data.setValue(i,1,height);
+		if (i == num_contenders/2){
+			data.setValue(i,2,annotation_num);
+			data.setValue(i,3,"Queen_of_Equestria used a 60-min leech attack!");
+			annotation_num++;
+		}
+		height = Math.floor(height*0.96);
 	}
-	/*
-	var change_events = document.getElementById('detailed_events').children;
-	for (i=0 ; i<change_events.length ; i++){
-		change_events[i].style.left = ;
+	for (var i=(3*num_contenders/4) ; i<num_contenders ; i++){
+		data.setValue(i,0,i+1);
+		data.setValue(i,1,height);
+		if (i == 3*num_contenders/4){
+			data.setValue(i,2,annotation_num);
+			data.setValue(i,3,"You used a power up of sorts!");
+			annotation_num++;
+		}
+		height = Math.floor(height*1.04);
 	}
-	*/
+	return data;
 }
