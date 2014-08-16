460e9463a2ff6326cd56282cbee1f3ffaf5d5971
kizyahaha <kczaja.ingersoll@gmail.com>
2014-08-15 22:07:52 -0500

    blah

--- web/scripts/global_vars.js
+++ web/scripts/global_vars.js
@@ -1,5 +1,5 @@
 var logged_user = get_user(); //logged_user of the currently logged in user
-var num_contenders = 100; //temporary variable only because we have no array of contenders yet
+var num_contenders = 70; //temporary variable only because we have no array of contenders yet
 var tot_cost_per_item = [0,0,0,0,0,0,0,0,0,0,0];  //how many boks are being spent for each item (quantity desired * price per).  amounts.length = number of special items
 var is_owner = false; //temporary until we can actually check for this
 
@@ -17,15 +17,6 @@ function get_user(){
 }
 
 function log_off_user(){
-	/*$.ajax({
-		async: false,
-		type: 'POST',
-		url: '/api/login/logout',
-		success: function(msg) {
-			logged_user = "";
-		    window.document.location.href = 'daily.html';
-		}
-	});*/
 	$.ajax({
 		async: false,
         type: 'POST',
