9ef1f7e6845149a7533e2b64ce48216bb9c76b2a
Bill Bindi <bbindi@palantir.com>
2014-09-20 05:08:01 -0700

    Minor refactoring of PowerSort.

--- src/main/java/com/kizy/filter/PowerSort.java
+++ src/main/java/com/kizy/filter/PowerSort.java
@@ -5,19 +5,26 @@ import java.util.Comparator;
 import java.util.List;
 
 import com.kizy.data.rant.Rant;
-import com.kizy.filter.PowerComparator;
 
 public class PowerSort implements Filter {
 
 	@Override
 	public List<Rant> doFilter(final List<Rant> rants, String arg) {
 		List<Rant> sortedRants = rants;
-		Comparator<Rant> powerComparator = new PowerComparator();
+		final int sign = isDescending(arg) ? -1 : 1;
+		Comparator<Rant> powerComparator = new Comparator<Rant>() {
+		    @Override
+		    public int compare(Rant rant1, Rant rant2){
+		        return (rant1.getRantPower() - rant2.getRantPower()) * sign;
+		    }
+		};
 		Collections.sort(sortedRants , powerComparator);
-		if (arg.equalsIgnoreCase("descending")){
-			Collections.reverse(sortedRants);
-		}
 		return sortedRants;
 	}
+
+    private boolean isDescending(String arg) {
+        return arg.equalsIgnoreCase("descending") ||
+               arg.equalsIgnoreCase("decreasing");
+    }
 
 }
